3aae575ae9e215d7b96f783a087a70cd3bebbe3f
Landon Hatch <landonhatch@gmail.com>
2019-11-21 23:13:46 -0700

    Removed unessesary data

--- screens/FoodSelectScreen.js
+++ screens/FoodSelectScreen.js
@@ -15,20 +15,8 @@ export default class FoodSelectScreen extends Component {
     constructor(props){
         super(props);
         this.state ={
-          dataSource: "",
-          text: "",
-          fdcid: "566851",
-          fat: "",
-          saturatedFat: "",
-          transFat: "",
-          cholesterol: "",
-          sodium: "",
-          carbohydrates: "",
-          fiber: "",
-          protein: "",
-          calcium: "",
-          iron: "",
-          calories: "",
+          fdcid: this.props.navigation.state.params.fdcid,
+          dataSource: {},
         }
       }
 
@@ -46,17 +34,7 @@ export default class FoodSelectScreen extends Component {
     
                 this.setState({
                   dataSource: responseJson,
-                  fat: responseJson.labelNutrients.fat.value.toString(),
-                  saturatedFat: responseJson.labelNutrients.saturatedFat.value.toString(),
-                  transFat: responseJson.labelNutrients.transFat.value.toString(),
-                  cholesterol: responseJson.labelNutrients.cholesterol.value.toString(),
-                  sodium: responseJson.labelNutrients.sodium.value.toString(),
-                  carbohydrates: responseJson.labelNutrients.carbohydrates.value.toString(),
-                  fiber: responseJson.labelNutrients.fiber.value.toString(),
-                  protein: responseJson.labelNutrients.protein.value.toString(),
-                  calcium: responseJson.labelNutrients.calcium.value.toString(),
-                  iron: responseJson.labelNutrients.iron.value.toString(),
-                  calories: responseJson.labelNutrients.calories.value.toString(),
+
               })
               //console.log(this.state.fat)
               })
@@ -66,111 +44,33 @@ export default class FoodSelectScreen extends Component {
         }
 
 addFood() {
-  
+
 }
           
 
 
   render() {
-
+      
     return (
       <Container>
+
 <Content>
 
-<View style={styles.searchHeader}>
-<Button onPress={() => this.addFood()}
-          style={styles.button}>
-            <Text>Add Food Item</Text>
-          </Button>
-          </View>
+<View style={styles.buttonPadder}>
+  <Button block onPress={() => this.addFood()}
+            style={styles.button}>
+              <Text>Add Food Item</Text>
+            </Button>
+</View>
 
-          <Card>
+          
+
+          <Card style={styles.card}>
+          <CardItem header>
+          <Text>{this.state.dataSource.description}</Text>
+            </CardItem>
             <CardItem header bordered>
               <Text>Nutrition Facts</Text>
-            </CardItem>
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Fat: {this.state.fat}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Saturated Fat: {this.state.saturatedFat}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Trans Fat: {this.state.transFat}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Cholesterol: {this.state.cholesterol}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Sodim: {this.state.sodium}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Carbohydrates: {this.state.carbohydrates}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Fiber: {this.state.fiber}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Protien: {this.state.protein}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Calcium: {this.state.calcium}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Iron: {this.state.iron}
-          </Text>
-        </Body>
-      </CardItem>     
-      <CardItem bordered>
-        <Body>
-          <Text>
-          Calories: {this.state.calories}
-          </Text>
-        </Body>
-      </CardItem>     
-          </Card>
-
-
-          <Card>
-            <CardItem header bordered>
-              <Text>Nutrients</Text>
             </CardItem>
       <FlatList
         data={this.state.dataSource.foodNutrients}
@@ -181,7 +81,7 @@ addFood() {
           </Text>
         </Body>
       </CardItem>}
-        keyExtractor={({id}, index) => id.toString()}
+        keyExtractor={item => item.nutrient.id.toString()}
       />        
           </Card>
         
@@ -198,10 +98,14 @@ addFood() {
 const styles = StyleSheet.create({
   button: {
     color: "white",
-    backgroundColor: "blue"
+    backgroundColor: "blue",
+    textAlign: "center"
   },
   searchBox: {
     paddingLeft: 20,
     paddingRight: 20,
+  },
+  buttonPadder: {
+    padding: 10
   }
 })
